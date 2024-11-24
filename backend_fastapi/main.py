ffe533c70449a40a34749284ab93fafff54dae3c
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-11-24 19:37:13 +0100

    finale

--- backend_fastapi/main.py
+++ backend_fastapi/main.py
@@ -16,6 +16,9 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 class TransactionBase(BaseModel):
@@ -42,7 +45,7 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 models.Base.metadata.create_all(bind=engine)
 
-@app.post("/transactions", response_model=TransactionModel, status_code=201)
+@app.post("/transactions/", response_model=TransactionModel, status_code=201)
 async def create_transaction(transaction: TransactionBase, db: db_dependency):
     db_transaction = models.Transactions(**transaction.model_dump())
     db.add(db_transaction)
@@ -51,7 +54,7 @@ async def create_transaction(transaction: TransactionBase, db: db_dependency):
     
     return db_transaction
 
-@app.get("/transactions", response_model=list[TransactionModel])
+@app.get("/transactions/", response_model=list[TransactionModel])
 async def read_transactions(db: db_dependency, skip: int=0, limit:int=100):
     transactions = db.query(models.Transactions).offset(skip).limit(limit).all()
     return transactions
